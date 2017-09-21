f5d566c800bf6588506c7621c90eed686c4e3b8a
Sergey Pepyakin <s.pepyakin@gmail.com>
2017-09-21 12:31:39 +0300

    Save staff

--- tests/headers/derive-partialeq-base.hpp
+++ tests/headers/derive-partialeq-base.hpp
@@ -1,8 +1,23 @@
-// bindgen-flags: --with-derive-partialeq --impl-partialeq
+// bindgen-flags: --with-derive-partialeq --impl-partialeq --opaque-type "Opaque"
+
+struct Opaque {
+    int i;
+    int large[33];
+};
 
 class Base {
     int large[33];
 };
 
 class ShouldDerivePartialEq: Base {
+};
+
+// This class and all it's subclasses can't derive PartialEq because
+// it contains opaque member.
+class BaseThatCantDerivePartialEq {
+    int large[33];
+    Opaque a;
+};
+
+class ShouldNotDerivePartialEq: Base, BaseThatCantDerivePartialEq {
 };
